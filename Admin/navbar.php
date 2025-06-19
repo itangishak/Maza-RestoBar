9de3b54419d8c3a3367a16eaadccfff7de855df0
Itangishaka J.Esterique <48064130+itangishak@users.noreply.github.com>
2025-06-19 12:33:45 +0200

    Fix topbar logo link

--- Admin/navbar.php
+++ Admin/navbar.php
@@ -1,6 +1,6 @@
 <header id="header" class="header fixed-top d-flex align-items-center">
   <div class="d-flex align-items-center justify-content-between">
-    <a href="#" class="logo d-flex align-items-center">
+    <a href="dashboard.php" class="logo d-flex align-items-center">
       <img src="../Client/img/LogoMaza.png" alt="logo" style="width: 100px;">
     </a>
     <i class="bi bi-list toggle-sidebar-btn"></i>
