a590f5947b2510c624a24c5c2e99dbc487acb331
Itangishaka J.Esterique <itangishakajohnesterique@gmail.com>
2025-07-15 14:42:56 +0200

    ch17

--- Admin/stock-dashboard.php
+++ Admin/stock-dashboard.php
@@ -585,9 +585,8 @@ if ($lowRes) {
 
   <?php include_once './footer.php'; ?>
 
-  <!-- Bootstrap JavaScript for dropdown functionality -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-  
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   <script>
     // Fix for navbar dropdown functionality - works offline
     function initializeNavbarDropdowns() {
